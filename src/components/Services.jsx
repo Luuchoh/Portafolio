82fd56bc33103005c787247bad04a0fe23d09a9a
Luuchoh <luxo997@gmail.com>
2021-08-05 00:22:14 -0500

    componentes sservices completados

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -4,41 +4,41 @@ export const Services = () => {
   return (
     <div className="container-service" id="service">
       <div className="service">
-        <h2 className ="text-capitalize font-weight-bold">servicios</h2>
+        <h2 className ="text-capitalize fw-bold mb-4">servicios</h2>
 
         <div className="row">
           <div className="col-4">
-              <h4>Diseño</h4>
+              <h4 className ="fw-bolder">Diseño</h4>
               <ul>
                   <li>Experiencia de usuario</li>
                   <li>Interfaz de usuario</li>
                   <li>Aplicaciones WEB</li>
                   <li>prueba de concepto</li>
               </ul>
-              <a href="/">
+              <a className="btn btn-dark" href="/">
                     Ver servicios de marca
                     <i class="bi bi-caret-right-fill"></i>
                 </a>
           </div>
           <div className="col-4">
-            <h4>Desarollo</h4>
+            <h4 className ="fw-bolder">Desarollo</h4>
               <ul>
                   <li>Aplicaciones moviles</li>
                   <li>Sitios WEB</li>
                   <li>Aplicaciones WEB progresivas</li>
               </ul>
-              <a href="/">
+              <a className="btn btn-dark" href="/">
                     Ver servicios de marca
                     <i class="bi bi-caret-right-fill"></i>
                 </a>
           </div>
           <div className="col-4">
-            <h4>Marca</h4>
+            <h4 className ="fw-bolder">Marca</h4>
               <ul>
                   <li>identidad de la marca</li>
                   <li>Estategia de marca</li>
               </ul>
-                <a href="/">
+                <a className="btn btn-dark" href="/">
                     Ver servicios de marca
                     <i class="bi bi-caret-right-fill"></i>
                 </a>
